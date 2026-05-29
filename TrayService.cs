05b5dd04edbd31499d37977fa8a7a863b4d7cdc5
Mauricio Partnoy <mauriciopartnoy@gmail.com>
2026-05-29 15:56:29 -0300

    v1.0.1 Test

--- TrayService.cs
+++ TrayService.cs
@@ -34,7 +34,7 @@ namespace GazeStream
         private ContextMenuStrip BuildMenu()
         {
             var menu = new ContextMenuStrip();
-            string version = $"Versión {App.CurrentVersion}" + "PRUEBA UPDATES";
+            string version = $"Versión {App.CurrentVersion}" + " =)";
             menu.Items.Add(version, null, Empty);
             menu.Items.Add(new ToolStripSeparator());
             menu.Items.Add("Reiniciar Eyetracker", null, OnRestartEyetracker);
